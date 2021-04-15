84cccf4bcd069f28eef6ad4649992710910d54bc
murikur2 <murikur2@uwm.edu>
2021-04-15 17:24:37 -0500

    index.html, URL, Views updated for base website

--- CS361_G3_TA_Scheduler/urls.py
+++ CS361_G3_TA_Scheduler/urls.py
@@ -1,7 +1,7 @@
-"""CS361_G3_TA_Scheduler URL Configuration
+"""mycontactdjangoProject1 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
+from django.urls import path
+from TA_Scheduler.views import Home
 
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('',Home.as_view())
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
