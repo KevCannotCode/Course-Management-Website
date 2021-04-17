88cdda7484b1f841191d8b8d2ff384791a30dafa
murikur2 <murikur2@uwm.edu>
2021-04-16 20:44:42 -0500

    Account Models Migrated

--- TA_Scheduler/models.py
+++ TA_Scheduler/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class myAccount(models.Model):
+    firstName = models.CharField(max_length=30)
+    lastName = models.CharField(max_length=30)
+    userName = models.CharField(max_length=40)
+    email = models.CharField(max_length=40)
+    phone = models.CharField(max_length=15)
+    userType = models.CharField(max_length=20)
+    password = models.CharField(max_length=40)
+
--- TA_Scheduler/views.py
+++ TA_Scheduler/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views import View
+from .models import myAccount
 # Create your views here.
 class Home(View):
     def get(self,request):
         return render(request,"index.html",{})
+    def get(self,request):
+        account_list = list(myAccount.objects.values_list("firstName","lastName","userName","email","phone","userType"))
+        return render(request, "index.html", {"account_list":account_list})
