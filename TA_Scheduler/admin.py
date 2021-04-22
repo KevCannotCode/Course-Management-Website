5723f6a2d8b6292a8defa05993dbfecbea63b143
Kevin <70717394+Kukalume@users.noreply.github.com>
2021-04-22 10:44:27 -0500

    added myLab and myCourse

--- TA_Scheduler/admin.py
+++ TA_Scheduler/admin.py
@@ -2,3 +2,5 @@ from django.contrib import admin
 from .models import myAccount
 # Register your models here.
 admin.site.register(myAccount)
+admin.site.register(myCourse)
+admin.site.register(myLab)
