261a568aee037ee926738add4bd2ceb847e2516c
murikur2 <murikur2@uwm.edu>
2021-04-22 16:26:13 -0500

    UI Update

--- CS361_G3_TA_Scheduler/settings.py
+++ CS361_G3_TA_Scheduler/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@sf_i%xch=dl@c!3u74*zcdayg&eki0u5wxo=l==i$2k%i^ysf'
+SECRET_KEY = 'django-insecure-9yc8v#$e8uxr4jas(%@!l09&df9#cu+wz60z=gt7c2*=#1#qwi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -120,7 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- TA_Scheduler/admin.py
+++ TA_Scheduler/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import myAccount
+from .models import myCourse
+from .models import myLab
+from .models import myContact
+from .models import myCourseInstructors
 # Register your models here.
 admin.site.register(myAccount)
+admin.site.register(myCourse)
+admin.site.register(myLab)
+admin.site.register(myContact)
+admin.site.register(myCourseInstructors)
