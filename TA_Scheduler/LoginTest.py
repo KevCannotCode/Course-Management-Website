c7d762015bfae942bf843ebe254baa6bcecf1267
Kukalume <kevinumba3007@gmail.com>
2021-04-22 15:20:58 -0500

    chnged the link from homepage to home

--- TA_Scheduler/LoginTest.py
+++ TA_Scheduler/LoginTest.py
@@ -1,60 +1,67 @@
 from django.test import TestCase
 from django.test import Client
-from .models import MyUser
-from .models import Stuff
+from .models import myAccount
 # Create your tests here.
 class login_test(TestCase):
     def setUp(self):
         #Setup client, account and something in the list
         self.client = Client()
-        self.adminUser = MyUser.objects.create(name="admin", password="password")
-        self.standardUser = MyUser.objects.create(name="dude", password = "pass")
-        self.nullsUser = MyUser.objects.create(name= None, password= None )
-        self.emptyUser = MyUser.objects.create(name= "", password= "" )
+        self.adminUser = myAccount.objects.create(name="admin", password="password")
+        self.standardUser = myAccount.objects.create(name="dude", password = "pass")
+        self.emptyUser = myAccount.objects.create(name= "", password= "" )
         #create a course
 
     def test_good_login(self):
+        #Good login with admin
+        response = self.client.post("/", {"name": self.adminUser.name, "password": self.adminUser.password})
+        #Check redirect: Should it redirect to a homepge when the user logs in?
+        self.assertEqual("/home", response.url,"Log in as admin with correct credentials admin failed."
+                                                    "Expected redirect URL to be /home")
         #Good login with standard user
-        response = self.client.post("/", {"name": , "password": adminUser})
-        #Check redirect
-        self.assertEqual("/things/", response.url,"Logging in as admin with correct password admin failed. Expected redirect URL to be /things/")
+        response = self.client.post("/", {"name": self.standardUser.name, "password": self.standardUser.password})
+        #Check redirect: Should it redirect to a homepge when the user logs in?
+        self.assertEqual("/home", response.url,"Log in a standard user with correct credentials admin failed."
+                                                    "Expected redirect URL to be /home")
 
+    def test_empty_credentials(self):
+        #Login with an empty username
+        response = self.client.post("/", {"name": self.standardUser.name, "password": self.emptyUser.password })
+        self.assertEqual("Please enter a password", response.context["message"],
+        "Log in with an empty password should fail. Expected <Please enter a password> message")
+        # Login with an empty password
+        response = self.client.post("/", {"name": self.emptyUser.name, "password": self.emptyUser.password})
+        self.assertEqual("Please enter a username", response.context["message"],
+                         "Log in with an empty password should fail. Expected <Please enter a username> message")
+        #login with empty username and password
+        response = self.client.post("/", {"name": self.emptyUser.name, "password": self.emptyUser.password})
+        self.assertEqual("Please enter a password and a username", response.context["message"],"Log in with an empty "
+        "password and username should fail. Expected <Please enter a password and a username> message")
 
+    def test_inexisting_credentials(self):
+        #Login with an inexisting username
+        response = self.client.post("/", {"name": "ret8578", "password": self.standardUser.password })
+        self.assertEqual("Incorrect username", response.context["message"],
+        "Log in with an incorrect username. Expected <Incorrect username> message")
+        # Login with an inexisting username
+        response = self.client.post("/", {"name": "ret8578", "password": self.adminUser.password})
+        self.assertEqual("Incorrect username", response.context["message"],
+                         "Log in with an incorrect username. Expected <Incorrect username> message")
 
+        # Login with an inexisting password
+        response = self.client.post("/", {"name": self.standardUser.name, "password": "eddee"})
+        self.assertEqual("Incorrect password", response.context["message"],
+                         "Log in with an incorrect password. Expected <Incorrect password> message")
+        # Login with an inexisting username
+        response = self.client.post("/", {"name": self.adminUser.name, "password": "eddee"})
+        self.assertEqual("Incorrect password", response.context["message"],
+                         "Log in with an incorrect password. Expected <Incorrect password> message")
 
-class TestCase_bad_login(TestCase):
-    def setUp(self):
-        # Setup client, account and something in the list
-        self.client = Client()
-        self.adminUser = MyUser.objects.create(name="admin", password="admin")
-        self.userThing = Stuff.objects.create(name="something", owner=self.adminUser)
-
-    def test_bad_login(self):
-        #Login with bad incorrect password
-        response = self.client.post("/", {"name": "admin", "password": "password"})
-        #Check message for bad password
-        self.assertEqual("bad password", response.context["message"], "Logging in as admin with wrong password password. Expected bad password message")
-
-
-
-
-class TestCase_list(TestCase):
-    def setUp(self):
-        # Setup client, account and something in the list
-        self.client = Client()
-        self.adminUser = MyUser.objects.create(name="admin", password="admin")
-        self.userThing = Stuff.objects.create(name="something", owner=self.adminUser)
-
-    def test_list(self):
-        #Login with adminUser
-        response1 = self.client.post("/", {"name": "admin", "password": "admin"})
-        #Check redirect
-        self.assertEqual("/things/", response1.url)
-        response = self.client.post("/things/", {"stuff": "new something"})
-        #Save list and length
-        listContent = response.context["things"]
-        listLen = len(listContent)
-        #Check length and value
-        print(listContent)
-        self.assertEqual(2, listLen)
-        self.assertEqual("new something", listContent[listLen-1], "Expected 'new something'. Adding to the list failed!")
+    def test_unmatching_credentials(self):#we test existing username and password that do not match
+        #Login with a standard username and admin password
+        response = self.client.post("/", {"name": self.standardUser.name, "password": self.standardUser.password })
+        self.assertEqual("Incorrect password", response.context["message"],
+        "Log in with an incorrect password. Expected <Incorrect password> message")
+        # Login with a standard password and admin username
+        response = self.client.post("/", {"name": self.adminUser.name, "password": self.standardUser.password})
+        self.assertEqual("Incorrect password", response.context["message"],
+                         "Log in with an incorrect password. Expected <Incorrect password> message")
