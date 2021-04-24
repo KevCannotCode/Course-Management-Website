5b313847d3f09cdbef08864189baeec33d6e320e
Kevin <70717394+Kukalume@users.noreply.github.com>
2021-04-24 15:12:40 -0500

    Update myLogin.py
    changed elfse if to elif

--- TA_Scheduler/myLogin.py
+++ TA_Scheduler/myLogin.py
@@ -8,18 +8,18 @@ class myLogin():
             #checking for long inputs
             if len(username) > 40:
                 errorMessage = "The username is too long"
-            else if  len(password) > 40 :
+            elif  len(password) > 40 :
                 errorMessage = "The password is too long"
             #checking for empty inputs
-            else if  len(username) < 1:
+            elif  len(username) < 1:
                 errorMessage = "The username is empty"
-            else if  len(password) < 1:
+            elif  len(password) < 1:
                 errorMessage = "The password is empty"
             #check if the username is non existant
             if errorMessage != "":
                 entry = myAccount.objects.get(userName = username)
             #check if the password matches the username
-            else if ( entry.password != password):
+            elif ( entry.password != password):
                 errorMessage = "The password does not match this username"
                 
         except myAccount.DoesNotExist:
