1bdc31479f7b770b6fcd435090494fe63fa5d0d7
Kevin <70717394+Kukalume@users.noreply.github.com>
2021-04-24 14:27:50 -0500

    Update Login.py

--- TA_Scheduler/Login.py
+++ TA_Scheduler/Login.py
@@ -5,9 +5,22 @@ class Login():
         # query the username as a pk
         errorMessage = ""
         try:
+            #checking for long inputs
+            if len(username) > 40:
+                errorMessage = "The username is too short"
+            if len(password) > 40 :
+                errorMessage = "The password is too long"
+            #checking for empty inputs
+            if len(username) < 1:
+                errorMessage = "The username empty"
+            if len(password) < 1:
+                errorMessage = "The password is empty"
+            #check if the username is non existant
             entry = myAccount.objects.get(username = username)
+            #check if the password matches the username
             if( entry.password is not password):
-                logged = False
+                errorMessage = "The password does not match this username"
+        #do you think i SHould check for the type?
         except myAccount.DoesNotExist:
             errorMessage = "The username was not found in the database"
         except myAccount.MultipleObjectsReturned:
