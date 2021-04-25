56e0880a81c40d80600b2848f5f057f7097d2519
Dan <deripka@uwm.edu>
2021-04-24 20:12:52 -0500

    Lab creation functions

--- TA_Scheduler/createLabFunctions.py
+++ TA_Scheduler/createLabFunctions.py
@@ -1,20 +1,38 @@
 from .models import myLab
 
-class createLabFunctions():
-    def createLab(labNumber, labName):
-        errorMessage = ""
-        if not (labNumber.isnumeric()):
-            errorMessage = "Lab Number Isn't Numeric"
-        else:
-            existingLab = list(myLab.objects.filter(labNumber = labNumber))
 
-            if (len(existingLab) != 0):
-                errorMessage = "Course Number Already Exists"
-            else:
-                errorMessage = ""
-                #createCourseFunctions.createCourse1(courseName, courseNumber)
-                l1 = myLab(labName= labName, labNumber= labNumber)
-                l1.save()
+class createLabFunctions:
+    def createLab(self, labName):  # self = labNumber
+        errorMessage = ""
+        if self == "" and labName == "":
+            errorMessage = "Lab name and number are empty"
+            return errorMessage
+        elif self == "":
+            errorMessage = "Lab number is empty"
+            return errorMessage
+        elif labName == "":
+            errorMessage = "Lab name is empty"
+            return errorMessage
+
+        if len(self) > 10:
+            errorMessage = "Lab Number Is Too Long!"
+            return errorMessage
+
+        if not (self.isnumeric()):
+            errorMessage = "Lab Number Isn't Numeric"
+            return errorMessage
+
+        existingLab = list(myLab.objects.filter(labNumber=self))
+
+        if len(existingLab) != 0:
+            errorMessage = "Lab Number Already Exists"
+            return errorMessage
+
+        if len(labName) > 20:
+            errorMessage = "Lab Name Is Too Long!"
+            return errorMessage
+
+        errorMessage = ""
+        l1 = myLab(labNumber=self, labName=labName)
+        l1.save()
         return errorMessage
-        
-        
