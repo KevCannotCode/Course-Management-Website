18ce4891244a37585d4b8a8171f0267d34b61915
Kevin <70717394+Kukalume@users.noreply.github.com>
2021-04-25 17:32:27 -0500

    Update ContactTest.py

--- TA_Scheduler/ContactTest.py
+++ TA_Scheduler/ContactTest.py
@@ -4,70 +4,71 @@ from .models import myContact
 from TA_Scheduler.Contact import Contact
 # Create your tests here.
 
-class TestContact(TestCase):
+# Create your tests here.
+class login_test(TestCase):
     def setUp(self):
+        # Setup client, account and number and email
         self.client = Client()
-        self.goodContact = Contact.createContact(4146142209, "good@uwm.edu")
-        self.emptyContact = Contact.createContact("", "")
+        self.adminUser = myContact.objects.create(number="4146968888", email="admin@uwm.edu")
+        self.standardUser = myContact.objects.create(number="4147774444", email="user@uwm.edu")
+        self.emptyUser = myContact.objects.create(number="", email="")
 
-    def create_good_contact(self):
-        response = self.client.post("/profile/", {"phoneNumber": "1", "courseName": "COMPSCI"})
-        course_list = response.context["course_list"]
-        coursePair = course_list[0]
-        self.assertEqual("1", str(coursePair[0]),
-                         "Creating a new course COMPSCI with number 1 failed. Expected course numbers to match. 1 = 1")
-        self.assertEqual("COMPSCI", str(coursePair[1]),
-                         "Creating a new course COMPSCI with number 1 failed. Expected course names to match. COMPSCI = COMPSCI")
-        self.assertEqual("", response.context["errorMessage"],
-                         "Creating a new course COMPSCI with number 1 failed. Expected errorMessage to be empty. '' = '' ")
+    def test_good_form(self):
+        # Good input with admin
+        response = self.client.post("/account/edit",
+                                         {"Number": self.adminUser.number, "email": self.adminUser.email})
+        self.assertEqual("Please enter a password and a username", response.context["message"],
+                         "Contact information updated: phone: 414-696-8888, email: admin@uwm.edu")
+        # contact with empty phone number and email
+        response = self.client.post("/account/edit",
+                                    {"Number": self.standardUser.number, "email": self.standardUser.email})
+        self.assertEqual("Please enter a password and a username", response.context["message"],
+                            "Contact information updated: phone: 414-777-4444, email: user@uwm.edu")
 
-        from django.test import TestCase
-        from django.test import Client
-        from .models import myContact
+    def test_empty_form(self):
+        # contact with empty phone number
+        response = self.client.post("/account/edit",
+                                            {"Number": self.emptyUser.number, "email": self.standardUser.email})
+        self.assertEqual("Please enter contact information", response.context["message"],
+                      "Phone number and Email are required",
+                   "Entering phone number is required to update account ")
+        # contact with empty email
+        response = self.client.post("/account/edit",
+                                    {"Number": self.standardUser.number, "email": self.emptyUser.email})
+        self.assertEqual("Please enter contact information", response.context["message"],
+                      "Phone number and Email are required", "Entering email is required to update account ")
+        # contact with empty phone number and email
 
-        # Create your tests here.
-        class login_test(TestCase):
-            def setUp(self):
-                # Setup client, account and number and email
-class loginUnitTest(TestCase):
-    def setUp(self):
-        self.client = Client()
-        self.adminUser = myAccount.objects.create(userName="admin", password="password")
-        self.standardUser = myAccount.objects.create(userName="dude", password="pass")
-        self.emptyUser = myAccount.objects.create(userName="", password="")
-        self.longUsername = "0123456789012345678901234567890123456789blablabla"
-        self.longPassword = "0123456789012345678901234567890123456789blablabla"
-    def good_login_test(self):
-        #the admin and standard username should be found in the database
-        self.assertEquals(myLogin.login(self.adminUser.userName, self.adminUser.password), "", "The credentials were correct but the login failed")
-        self.assertEquals(myLogin.login(self.standardUser.userName, self.standardUser.password), "", "The credentials were correct but the login failed")
+        response = self.client.post("/account/edit",
+                                    {"Number": self.emptyUser.number, "email": self.emptyUser.email})
+        self.assertEqual("Please enter contact information", response.context["message"],
+                "Phone number and Email are required", "Entering contact information is required to update account.")
 
-    def long_inputs_test(self):
-        #long username
-        self.assertEquals(myLogin.login(self.longUsername, self.adminUser.password), "The Username Is Too Long!",  "The username was wrong but login did not fail")
-        self.assertEquals(myLogin.login(self.longUsername, self.standardUser.password), "The Username Is Too Long!", "The username was wrong but login did not fail")
-        #long password
-        self.assertEquals(myLogin.login(self.adminUser.userName, self.longPassword), "The Password Is Too Long!",  "The username was wrong but login did not fail")
-        self.assertEquals(myLogin.login(self.standardUser.userName, self.longPassword),"The Password Is Too Long!", "The username was wrong but login did not fail")
-        #both inputs are too long
-        self.assertEquals(myLogin.login(self.longUsername, self.longPassword),"The Username Is Too Long!",  "The username was wrong but login did not fail")
+    def test_BadInput_Contact(self):  # we test existing username and password that do not match
+        # Incorrect Email format
+        response = self.client.post("/account/edit", {"1412005555": self.standardUser.number,
+                "useratuwmedu": self.standardUser.email})
+        self.assertEqual("Incorrect email format", response.context["message"],
+                "Incorrect Email Format, an example of the correct format is example@website.org.  Expected <Incorrect email> message")
+        # Login with a standard password and admin username
+        response = self.client.post("/account/edit", {"151561651561666": self.adminUser.number,
+                                        "email@uwm.edu": self.standardUser.email})
+        self.assertEqual("Incorrect number format", response.context["message"],
+        "Incorrect number Format, an example of the correct format is 4140001111.  Expected <Incorrect number> message")
 
-    def wrong_username_test(self):
-        self.assertEquals(myLogin.login("wrong", self.adminUser.password), "User doesn't exist",  "The username was wrong but login did not fail")
-        self.assertEquals(myLogin.login("wrong", self.standardUser.password),"User doesn't exist" , "The username was wrong but login did not fail")
-
-    def wrong_password_test(self):
-        self.assertEquals(myLogin.login(self.standardUser.userName, "wrong" ),"Incorrect Password!" , "The password was wrong but login worked")
-        self.assertEquals(myLogin.login(self.adminUser.userName, "wrong" ), "Incorrect Password!","The password was wrong but login worked")
-
-
-    def empty_inputs_test(self):
-        #empty username
-        self.assertEquals("No Username Provided", myLogin.login(self.emptyUser.userName, self.emptyUser.password),"The username was empty but login worked")
-        #empty password
-        self.assertEquals("No Password Provided", myLogin.login(self.standardUser.userName, self.standardUser.password), "The password was empty but  worked")
-        #both inputs empty
-        self.assertEquals("No Username Provided", myLogin.login(self.emptydUser.userName, self.emptyUser.password), "Both inputs were empty but login worked")
-
-    def input_mismatch_test(self):
-        self.assertEquals("Incorrect Password", myLogin.login(self.adminUser.userName, self.standardUser.password),"The inputs don't match but login worked")
+    def duplicate_info_Contact(self):  # testing for contact info already linked to another account
+        # Duplicate phone numbers
+        response = self.client.post("/account/edit", {"4146968888": self.standardUser.number,
+                                            "email@uwm.edu": self.standardUser.email})
+        self.assertEqual("Duplicate Phone Number", response.context["message"],
+                     "Phone Number already belongs to another account. Check with admin or try another Expected Duplicate number message")
+        # Duplicate emails
+        response = self.client.post("/account/edit", {"1111111111": self.adminUser.number,
+                        "user@uwm.edu": self.standardUser.email})
+        self.assertEqual("Duplicate Email", response.context["message"],
+           "Email already belongs to another account. Check with admin or try another Expected Duplicate email message")
+        # Both duplicate
+        response = self.client.post("/account/edit", {"4146968888": self.adminUser.number,
+                         "user@uwm.edu": self.standardUser.email})
+        self.assertEqual("Both Phone and Email are duplicate", response.context["message"],
+        "Both Phone and Email are already connected to another account. Expected Duplicate number and email message")
