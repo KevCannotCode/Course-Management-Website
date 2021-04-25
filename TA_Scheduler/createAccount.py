7c6eeed649c8197ef27e11af83b2ee8cc6287582
murikur2 <murikur2@uwm.edu>
2021-04-24 22:48:19 -0500

    verifyLogin.py Added

--- TA_Scheduler/createAccount.py
+++ TA_Scheduler/createAccount.py
@@ -1,4 +1,5 @@
 from .models import myAccount
+from .models import myContact
 
 class createAccountFunctions():
     def createAccount(userName, password):
@@ -22,13 +23,26 @@ class createAccountFunctions():
             return errorMessage
         else:
             existingAccounts = list(myAccount.objects.filter(userName=userName))
+            existingContacts = list(myContact.objects.filter(userName=userName))
 
         #check database to see if there is an account that already exists
         if (len(existingAccounts) != 0):
             errorMessage = "Username Already Exists!"
+            return errorMessage
+
+        if (len(existingContacts) != 0):
+            existingContact = existingContacts[0]
+            existingContact.phoneNumber = "NOT SET"
+            existingContact.emailAddress = "NOT SET"
+            existingContact.save()
+            newAccount = myAccount(userName=userName, password=password)
+            newAccount.save()
+
         else:
             #creates new account with inputted fields and saves it to the database
             newAccount = myAccount(userName=userName, password=password)
             newAccount.save()
+            newContact = myContact(userName=userName, phoneNumber="NOT SET", emailAddress="NOT SET")
+            newContact.save()
 
         return errorMessage
--- TA_Scheduler/views.py
+++ TA_Scheduler/views.py
@@ -9,6 +9,7 @@ from .createLabFunctions import createLabFunctions
 from .createAccount import createAccountFunctions
 from .myLogin import myLogin
 from django.contrib.auth import logout as auth_logout
+from .verifyLogin import verifyLogin
 
 # Create your views here.
 class Login(View):
@@ -22,13 +23,14 @@ class Login(View):
         return render(request,"index.html",{})
 
     def post(self,request):
-        errorMessage = myLogin.login(request.POST['userName'], request.POST['password'])
-        if(errorMessage != ""):
-            return render(request, "index.html", {"errorMessage": errorMessage})
-        else:
-            m = myAccount.objects.get(userName=request.POST['userName'])
-            request.session["userName"] = m.userName
-            return redirect("/home/")
+        if len(request.POST) != 0:
+            errorMessage = myLogin.login(request.POST['userName'], request.POST['password'])
+            if(errorMessage != ""):
+                return render(request, "index.html", {"errorMessage": errorMessage})
+            else:
+                m = myAccount.objects.get(userName=request.POST['userName'])
+                request.session["userName"] = m.userName
+                return redirect("/home/")
 
 class Logout(View):
     def get(self,request):
@@ -38,6 +40,8 @@ class Logout(View):
 class Home(View):
     def get(self,request):
         userName = request.session["userName"]
+        if(verifyLogin.verifyLogin(userName, request) == False):
+            return render(request, "logout.html", {})
 
         account_list = list(myAccount.objects.values_list("userName", "password"))
         course_list = list(myCourse.objects.values_list("courseNumber", "courseName"))
@@ -50,24 +54,33 @@ class Home(View):
 class CreateAccount(View):
     def get(self,request):
         userName = request.session["userName"]
+        if(verifyLogin.verifyLogin(userName, request) == False):
+            return render(request, "logout.html", {})
         account_list = list(myAccount.objects.values_list("userName", "password"))
         return render(request,"create-account.html",{"account_list":account_list, "userName":userName, "errorMessage":""})
 
     def post(self,request):
-        userName = request.session["userName"]
-        errorMessage = createAccountFunctions.createAccount(request.POST["userName"], request.POST["password"])
-        account_list = list(myAccount.objects.values_list("userName", "password"))
-        return render(request,"create-account.html",{"account_list":account_list, "userName":userName, "errorMessage":errorMessage})
+        if len(request.POST) != 0:
+            userName = request.session["userName"]
+            if(verifyLogin.verifyLogin(userName, request) == False):
+                return render(request, "logout.html", {})
+            errorMessage = createAccountFunctions.createAccount(request.POST["userName"], request.POST["password"])
+            account_list = list(myAccount.objects.values_list("userName", "password"))
+            return render(request,"create-account.html",{"account_list":account_list, "userName":userName, "errorMessage":errorMessage})
 
 class CreateCourse(View):
     def get(self,request):
         userName = request.session["userName"]
+        if(verifyLogin.verifyLogin(userName, request) == False):
+            return render(request, "logout.html", {})
         course_list = list(myCourse.objects.values_list("courseNumber", "courseName"))
         return render(request,"create-course.html",{"course_list":course_list, "userName":userName, "errorMessage":""})
 
     def post(self,request):
         if len(request.POST) != 0:
             userName = request.session["userName"]
+            if (verifyLogin.verifyLogin(userName, request) == False):
+                return render(request, "logout.html", {})
             errorMessage = createCourseFunctions.createCourse(request.POST["courseNumber"], request.POST["courseName"])
             course_list = list(myCourse.objects.values_list("courseNumber","courseName"))
             return render(request,"create-course.html", {"course_list":course_list, "errorMessage":errorMessage, "userName":userName})
@@ -76,12 +89,16 @@ class CreateCourse(View):
 class CreateLab(View):
     def get(self,request):
         userName = request.session["userName"]
+        if(verifyLogin.verifyLogin(userName, request) == False):
+            return render(request, "logout.html", {})
         lab_list = list(myLab.objects.values_list("labNumber", "labName"))
         return render(request,"create-lab.html",{"lab_list":lab_list, "userName":userName, "errorMessage":""})
 
     def post(self,request):
         if len(request.POST) != 0:
             userName = request.session["userName"]
+            if (verifyLogin.verifyLogin(userName, request) == False):
+                return render(request, "logout.html", {})
             errorMessage = createLabFunctions.createLab(request.POST["labNumber"], request.POST["labName"])
             lab_list = list(myLab.objects.values_list("labNumber","labName"))
             return render(request,"create-lab.html", {"lab_list":lab_list, "errorMessage":errorMessage, "userName":userName})
@@ -89,6 +106,8 @@ class CreateLab(View):
 class Profile(View):
     def get(self,request):
         userName = request.session["userName"]
+        if(verifyLogin.verifyLogin(userName, request) == False):
+            return render(request, "logout.html", {})
 
         if(len(list(myContact.objects.filter(userName=userName))) == 0):
             myContact.objects.create(userName=userName, phoneNumber="NOT SET", emailAddress="NOT SET")
@@ -97,13 +116,16 @@ class Profile(View):
         return render(request, "profile.html", {"userName":userName, "phoneNumber":contact.phoneNumber, "emailAddress":contact.emailAddress})
 
     def post(self,request):
-        userName = request.session["userName"]
+        if len(request.POST) != 0:
+            userName = request.session["userName"]
+            if(verifyLogin.verifyLogin(userName, request) == False):
+                return render(request, "logout.html", {})
 
-        if(len(list(myContact.objects.filter(userName=userName))) == 0):
-            myContact.objects.create(userName=userName, phoneNumber="NOT SET", emailAddress="NOT SET")
+            if(len(list(myContact.objects.filter(userName=userName))) == 0):
+                myContact.objects.create(userName=userName, phoneNumber="NOT SET", emailAddress="NOT SET")
 
-        contact = list(myContact.objects.filter(userName=userName))[0]
-        contact.phoneNumber = request.POST["phoneNumber"]
-        contact.emailAddress = request.POST["emailAddress"]
-        contact.save()
-        return render(request, "profile.html", {"userName":userName, "phoneNumber":contact.phoneNumber, "emailAddress":contact.emailAddress})
+            contact = list(myContact.objects.filter(userName=userName))[0]
+            contact.phoneNumber = request.POST["phoneNumber"]
+            contact.emailAddress = request.POST["emailAddress"]
+            contact.save()
+            return render(request, "profile.html", {"userName":userName, "phoneNumber":contact.phoneNumber, "emailAddress":contact.emailAddress})
