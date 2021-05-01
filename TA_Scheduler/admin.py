b5fa6f0912d05fcdfb2d8a7cedb56dcad212eb08
murikur2 <murikur2@uwm.edu>
2021-05-01 14:30:08 -0500

    Models updated

--- TA_Scheduler/admin.py
+++ TA_Scheduler/admin.py
@@ -3,10 +3,14 @@ from .models import myAccount
 from .models import myCourse
 from .models import myLab
 from .models import myContact
-from .models import myCourseInstructors
+from .models import myCourseInstructor
+from .models import myLabTA
+from .models import labToCourse
 # Register your models here.
 admin.site.register(myAccount)
 admin.site.register(myCourse)
 admin.site.register(myLab)
 admin.site.register(myContact)
-admin.site.register(myCourseInstructors)
+admin.site.register(myCourseInstructor)
+admin.site.register(myLabTA)
+admin.site.register(labToCourse)
--- TA_Scheduler/models.py
+++ TA_Scheduler/models.py
@@ -5,20 +5,33 @@ from django.db import models
 class myAccount(models.Model):
     userName = models.CharField(max_length=40)
     password = models.CharField(max_length=40)
+    userType = models.CharField(max_length=40)
 
 class myCourse(models.Model):
     courseName = models.CharField(max_length=20)
     courseNumber = models.IntegerField()
+    instructorUserName = models.CharField(max_length=40)
+
 
 class myLab(models.Model):
     labName = models.CharField(max_length=20)
     labNumber = models.IntegerField()
+    taUserName = models.CharField(max_length=40)
 
 class myContact(models.Model):
     userName = models.CharField(max_length=40)
     phoneNumber = models.CharField(max_length=20)
     emailAddress = models.CharField(max_length=40)
 
-class myCourseInstructors(models.Model):
+class myCourseInstructor(models.Model):
     courseNumber = models.IntegerField()
-    userName = models.CharField(max_length=40)
+    instructorUserName = models.CharField(max_length=40)
+
+
+class myLabTA(models.Model):
+    labNumber = models.IntegerField()
+    taUserName = models.CharField(max_length=40)
+
+class labToCourse(models.Model):
+    labNumber = models.IntegerField()
+    courseNumber = models.IntegerField()
