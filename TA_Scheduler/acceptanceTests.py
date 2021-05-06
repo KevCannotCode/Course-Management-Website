657e25e93fb9adb58fd3f4348fd64f247abe5341
Cj shult <cjshult@uwm.edu>
2021-05-06 16:00:16 -0500

    CourseInstructor class, and start of unit/acceptance tests for CourseInstructor

--- TA_Scheduler/acceptanceTests.py
+++ TA_Scheduler/acceptanceTests.py
@@ -313,3 +313,88 @@ class testSectionEmptyInput(TestCase):
         self.assertEqual("No Lab Number or Lab Name Provided!", response.context["errorMessage"],
                          "Error creating new section, both labNumber and labName are empty")
 
+
+
+class myCourseInstructor(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.course = myCourseInstructor.objects.create(instructorUsername= "admin", courseNumber= 1)
+
+    def test_good_assign(self):
+        resp = self.client.post("/myCourseInstructor/", {"Instructor Name": self.myCourseInstructor.instructorUserName,
+                                                         "CourseNumber": self.myCourseInstructor.courseNumber})
+        self.assertEquals(resp.context["message"], "Course Assigned", "assigning instructor to course failed."
+                                                                   "Expected <Course Assigned> message")
+
+    def test_empty_arguments(self):
+        #empty labNumber
+        resp = self.client.post("/assign-LabToCourse/", {"Lab Number": "", "course Number": self.course.courseNumber})
+        self.assertEquals(resp.context["message"], "Lab Number Not Provided!",
+                          "Assigning lab to course failed. Expected the message <Lab Number Not Provided!>")
+        # empty courseNumber
+        resp = self.client.post("/assign-LabToCourse/", {"Lab Number": self.lab.labNumber, "course Number":""})
+        self.assertEquals(resp.context["message"], "Lab Number Not Provided!",
+                          "Assigning lab to course failed. Expected the message <Lab Number Not Provided!>")
+        # empty labNumber and courseNumber
+        resp = self.client.post("/assign-LabToCourse/", {"Lab Number": "", "course Number": ""})
+        self.assertEquals(resp.context["message"], "No input Provided!",
+                          "Assigning lab to course failed. Expected the message <No input Provided!>")
+
+    def test_long_arguments(self):
+        #too long lab number
+        resp = self.client.post("/assign-LabToCourse/", {"Lab Number": "7410", "course Number":self.course.courseNumber})
+        self.assertEquals(resp.context["message"], "Lab Number Not Provided!",
+                          "Assigning lab to course failed. Expected the message <Lab Number Not Provided!>")
+        # too long course number
+        resp = self.client.post("/assign-LabToCourse/", {"Lab Number": self.lab.labNumber,"course Number": "7410"})
+        self.assertEquals(resp.context["message"], "Course Number Not Provided!",
+                          "Assigning lab to course failed. Expected the message <Course Number Not Provided!>")
+        # too long lab number and too long course number
+        resp = self.client.post("/assign-LabToCourse/", {"Lab Number": "7410", "course Number": "7410"})
+        self.assertEquals(resp.context["message"], "Course Number Not Provided!",
+                          "Assigning lab to course failed. Expected the message <Course Number Not Provided!>")
+
+    def test_max_numbers(self):
+        # max integer lab number
+        resp = self.client.post("/assign-LabToCourse/",
+                                {"Lab Number": sys.maxsize, "course Number": self.course.courseNumber})
+        self.assertEquals(resp.context["message"], "Lab Number Not Provided!",
+                          "Assigning lab to course failed. Expected the message <Lab Number Not Provided!>")
+        # max integer course number
+        resp = self.client.post("/assign-LabToCourse/", {"Lab Number": self.lab.labNumber, "course Number":sys.maxsize})
+        self.assertEquals(resp.context["message"], "Course Number Not Provided!",
+                          "Assigning lab to course failed. Expected the message <Course Number Not Provided!>")
+        # max integer lab number and max integer course number
+        resp = self.client.post("/assign-LabToCourse/", {"Lab Number": sys.maxsize, "course Number": sys.maxsize})
+        self.assertEquals(resp.context["message"], "Course Number Not Provided!",
+                          "Assigning lab to course failed. Expected the message <Course Number Not Provided!>")
+        # min integer lab number
+        resp = self.client.post("/assign-LabToCourse/",
+                                {"Lab Number": -sys.maxsize+1, "course Number": self.course.courseNumber})
+        self.assertEquals(resp.context["message"], "Lab Number Not Provided!",
+                          "Assigning lab to course failed. Expected the message <Lab Number Not Provided!>")
+        # min integer course number
+        resp = self.client.post("/assign-LabToCourse/",
+                                {"Lab Number": self.lab.labNumber, "course Number": -sys.maxsize+1})
+        self.assertEquals(resp.context["message"], "Course Number Not Provided!",
+                          "Assigning lab to course failed. Expected the message <Course Number Not Provided!>")
+        # min integer lab number and min integer course number
+        resp = self.client.post("/assign-LabToCourse/", {"Lab Number": -sys.maxsize+1, "course Number": -sys.maxsize+1})
+        self.assertEquals(resp.context["message"], "Course Number Not Provided!",
+                          "Assigning lab to course failed. Expected the message <Course Number Not Provided!>")
+
+    def test_nonexisting_inputs(self):
+        # correct but non existing lab number
+        resp = self.client.post("/assign-LabToCourse/",
+                                {"Lab Number": "999", "course Number": self.course.courseNumber})
+        self.assertEquals(resp.context["message"], "This Lab Doesn't Exist!",
+                          "Assigning lab to course failed. Expected the message <This Lab Doesn't Exist!>")
+        # correct but non existing course number
+        resp = self.client.post("/assign-LabToCourse/",
+                                {"Lab Number": self.lab.labNumber, "course Number": "999"})
+        self.assertEquals(resp.context["message"], "This Course Doesn't Exist!",
+                          "Assigning lab to course failed. Expected the message <This Course Doesn't Exist!!>")
+        # non existing course and lab number
+        resp = self.client.post("/assign-LabToCourse/", {"Lab Number": "999", "course Number": "999"})
+        self.assertEquals(resp.context["message"], "This Lab Doesn't Exist!",
+                          "Assigning lab to course failed. Expected the message <This Lab Doesn't Exist!>")
