b42ee18d5e441a4990a2c53bfe1d876814fa849c
Kukalume <kevinumba3007@gmail.com>
2021-05-08 02:28:47 -0500

    bad commit

---
+++ TA_Scheduler/assignLabTA.py
@@ -0,0 +1,8 @@
+from .models import myLabTA
+from .models import myLab
+from .models import myAccount
+
+class assignLabTA:
+    def assignLabToTA(labNumber, taUSername):
+        errorMessage = ""
+        return errorMessage
