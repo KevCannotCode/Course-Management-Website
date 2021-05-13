ad0855cfcd334925e5db592e8e527faa9a5bbac8
Kevin <70717394+Kukalume@users.noreply.github.com>
2021-05-13 02:57:04 -0500

    Update myCourseInstructor.py
    updated and fixed some tests

--- TA_Scheduler/myCourseInstructor.py
+++ TA_Scheduler/myCourseInstructor.py
@@ -1,30 +1,28 @@
 from django.test import TestCase
 from django.test import Client
 from .models import myCourseInstructor
-
+from .models import myAccount
+from .models import myCourse
 
 class myCourseInstructor:
     def myCourseInstructor( instructorUserName , courseNumber):
         # check the validity of input
         errorMessage = myCourseInstructor.inputValidation(instructorUserName, courseNumber)
         # check if the instructor and course already exist
-        errorMessage = myCourseInstructor.DatabaseCheck(instructorUserName, courseNumber)
+        if errorMessage == "":
+            errorMessage = myCourseInstructor.DatabaseCheck(instructorUserName, courseNumber)
 
         if errorMessage == "":
-            # checks if there is an existing courseInstructor entry
-            entry = list(myCourseInstructor.objects.filter(instructorUserName=instructorUserName, courseNumber=courseNumber))
-            if len(entry) != 0:
-                errorMessage = "This instructor and course is assigned already"
-                return errorMessage
-            # if there is no existing entry, then we can assign it
             assign = myCourseInstructor(instructorUserName=instructorUserName, courseNumber=courseNumber)
             assign.save()
-
         return errorMessage
 
     def inputValidation(instructorUserName, courseNumber):
         errorMessage = ""
         # Check for empty inputs
+        if len(instructorUserName) < 1 and len(courseNumber) < 1:
+            errorMessage = "No Input Provided!"
+            return errorMessage
         if len(instructorUserName) < 1:
             errorMessage = "Instructor name is required"
             return errorMessage
@@ -34,7 +32,7 @@ class myCourseInstructor:
 
         # Check for long inputs
         elif len(instructorUserName) > 40:
-            errorMessage = "The Lab Number Is Too Long!"
+            errorMessage = "Instructor Name is Too long!"
             return errorMessage
 
         elif len(courseNumber) > 3:
@@ -43,22 +41,34 @@ class myCourseInstructor:
 
         # Check for numeric input
         elif not (courseNumber.isnumeric()):
-            errorMessage = "The course number isn't a number"
+            errorMessage = "The Course Number Isn't Numeric!"
         return errorMessage
 
     def DatabaseCheck(instructorUserName, courseNumber):
         errorMessage = ""
-        try:
-            instructor = myCourseInstructor.objects.filter(instructorUserName=instructorUserName)
-        except myCourseInstructor.DoesNotExist:
-            errorMessage = "The instructor does not exist"
-        try:
-            course = myCourseInstructor.objects.filter(courseNumber=courseNumber)
-        except myCourseInstructor.DoesNotExist:
-            errorMessage = "The course does not exist"
-        finally:
-            return errorMessage
 
+        instructor = myAccount.objects.filter(userName=instructorUserName)
+        if len(instructor) < 1:
+            errorMessage = "The instructor does not exist"
+            return errorMessage
+        course = myCourse.objects.filter(courseNumber=courseNumber)
+        if len(course) < 1:
+            errorMessage = "This Course Doesn't Exist"
+            return errorMessage
+        instructor = myCourseInstructor.objects.filter(instructorUserName= instructorUserName)
+        if len(instructor) < 1:
+            errorMessage = "The Instructor Has Already Been assigned!"
+            return errorMessage
+        course = myCourseInstructor.objects.filter(courseNumber=courseNumber)
+        if len(course) < 1:
+            errorMessage = "This Course Has Already Been Assigned!"
+            return errorMessage
+        instructorToCourse = myCourseInstructor.objects.filter(instructorUserName= instructorUserName
+                                                               ,courseNumber= courseNumber)
+        if len(instructorToCourse) < 1:
+            errorMessage = "This Instructor Has Already Been Assigned To This Course!"
+            return errorMessage
+        return errorMessage
 
 
 
